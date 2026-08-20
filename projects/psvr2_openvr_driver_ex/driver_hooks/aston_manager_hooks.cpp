05bb3fbe47fba948ac3312e158018f8706515618
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-08-20 20:31:35 -0300

    Remove stub for controller poll rate on Linux
    This is no longer necessary with a patched BlueZ.

--- projects/psvr2_openvr_driver_ex/driver_hooks/aston_manager_hooks.cpp
+++ projects/psvr2_openvr_driver_ex/driver_hooks/aston_manager_hooks.cpp
@@ -8,12 +8,6 @@ namespace psvr2_toolkit {
 void AstonManagerHooks::InstallHooks() {
   static HmdDriverLoader *pHmdDriverLoader = HmdDriverLoader::Instance();
 
-  // Controller poll rate stub
-  if (Util::IsRunningOnWine()) {
-    Util::DriverLog("Stubbing controller poll rate update due to Bluetooth limitations.");
-    HookLib::InstallStub(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x1cdff0));
-  }
-
   // AstonManager::getFileFirmwareVersion
   HookLib::InstallStub(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x119400));
 }
